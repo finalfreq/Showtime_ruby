f2fb2683fcecec13efbb0262fbae179f47458445
james robert williams & stephen <student@epicodus.com>
2015-05-07 11:32:44 -0700

    update sinatra to include movie info page and actor info page as well as use movies_actors table to show which actors were in which movie

--- app.rb
+++ app.rb
@@ -41,6 +41,40 @@ post('/movies') do
 end
 
 get('/actors/:id') do
-	@actor = Actor.find(params.fetch('id').to_i()
-	@movies = Movie.all()	
+	@actor = Actor.find(params.fetch('id'))
+	@movies = Movie.all()
+	erb(:actor_info)
+end
+
+patch('/actors/:id') do
+	actor_id = params.fetch('id')
+	@actor = Actor.find(actor_id)
+	movie_ids = params.fetch("movie_ids")
+	@actor.update(movie_ids: movie_ids)
+	@movies = Movie.all()
+  erb(:actor_info)
+end
+
+get('/movies/:id') do
+	@movie = Movie.find(params.fetch('id'))
+	@actors = Actor.all()
+	erb(:movie_info)
+end
+
+patch('/movies/:id') do
+	movie_id = params.fetch('id')
+	actor_ids = params.fetch('actor_ids')
+	@movie = Movie.find(movie_id)
+	@movie.update({:actor_ids => actor_ids})
+	@actors = Actor.all()
+	erb(:movie_info)
+end
+
+delete('/actors/:id') do
+	actor_id = params.fetch('id')
+	@actor = Actor.find(actor_id)
+	movie_ids = params.fetch("movie_ids")
+	@actor.delete(movie_ids: movie_ids)
+	@movies = Movie.all()
+	erb(:actor_info)
 end
